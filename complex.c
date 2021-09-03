0722ab111c4cf9c99d2cafe52a2f644261e23826
Maynard <maynard@lclark.edu>
2021-09-03 14:41:11 -0700

    most of class is done, added notes on complex numbers and couple c things <3

--- complex.c
+++ complex.c
@@ -5,10 +5,10 @@ struct cplx {
     double im;
 };
 
-void cadd(struct cplx arg1, struct cplx arg2, struct cplx res)
+void cadd(struct cplx arg1, struct cplx arg2, struct cplx *res)
 {
-    res.re = arg1.re + arg2.re;
-    res.im = arg1.im + arg2.im;
+    res->re = arg1.re + arg2.re;
+    res->im = arg1.im + arg2.im;
 }
 
 int main()
@@ -17,7 +17,7 @@ int main()
     struct cplx c2 = { 3.2, -1.2 };
     struct cplx c3;
 
-    cadd(c1, c2, c3);
+    cadd(c1, c2, &c3);
 
     printf("c3 = %f + %f i\n", c3.re, c3.im);
 }
