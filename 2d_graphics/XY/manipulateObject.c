dcffead3eb004e26f12b16d9f619c3812eb82794
Maynard <maynard@lclark.edu>
2021-10-14 09:44:49 -0700

    small updates to manipulateObject <3

--- 2d_graphics/XY/manipulateObject.c
+++ 2d_graphics/XY/manipulateObject.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include "M2d_matrix_toolsS.c"
+#include "../M2d_matrix_toolsS.c"
 
 int manualInput()
 {
@@ -78,9 +78,95 @@ int manualInput()
   printf("\n");
   
   return 1;
+}
 
+int automaticInput(){
+
+  char* filePath;
+  printf("Enter a file path: ");
+  scanf("%s",filePath);
+
+  FILE *f;
+  f = fopen(filePath,"r");
+  if(f == NULL){
+    printf("Cannot open file!\nEnter a new file: ");
+    scanf("%s",filePath);
+    
+    f = fopen(filePath,"r");
+    if(f == NULL){
+      printf("File still invalid... Exiting...\n");
+      return 0;
+    }
+  }
+  
+  printf("Loading file \"%s\"\n",filePath);
+
+  int numpoints;
+  fscanf(f,"%d",&numpoints);
+
+  double x[numpoints],y[numpoints];
+  for(int i = 0; i < numpoints; i++){
+    fscanf(f,"%lf %lf",&x[i],&y[i]);
+  }
+
+  printf("\nFile Load Complete!\nEnter first translation (t: translate, r: rotate, s: scale, f:finish):");
+  char command;
+  double a[3][3];
+  double b[3][3];
+  double vectorA, vectorB;
+  M2d_make_identity(a);
+  
+  scanf(" %c",&command);
+
+  while(command != 'f'){
+
+    if(command == 't'){
+      printf("Enter vectors (x y): ");
+      scanf("%lf %lf",&vectorA, &vectorB);
+      M2d_make_translation(b, vectorA, vectorB);
+      M2d_mat_mult(a,b,a);
+
+    }
+    else if (command == 'r'){
+      printf("Enter degrees (deg): ");
+      scanf("%lf",&vectorA);
+      M2d_make_rotation(b, vectorA*M_PI/180);
+      M2d_mat_mult(a,b,a);
+    }
+    else if(command == 's'){
+      printf("Enter scalers (x y): ");
+      scanf("%lf %lf",&vectorA, &vectorB);
+      M2d_make_scaling(b, vectorA, vectorB);
+      M2d_mat_mult(a,b,a);
+    }
+
+    printf("\nThank you! Enter command:\n(t: translate, r: rotate, s: scale, f:finish): ");
+    scanf(" %c",&command);
+  }
+
+  printf("\n Thank you for your commands!\n");
+
+  printf("\nHere's your delta matrix! \n");
+  M2d_print_mat(a);
+  printf("\n");
+
+  printf("Here's your new values:\n");
+
+  M2d_mat_mult_points(x, y, a, x, y, numpoints);
+  
+  printf("X: ");
+  for(int i = 0; i < numpoints; i++){
+    printf("%lf ",x[i]);
+  }
+  printf("\n");
+
+  printf("Y: ");
+  for(int i = 0; i < numpoints; i++){
+    printf("%lf ",y[i]);
+  }
+  printf("\n");
+  
   return 1;
-
 }
 
 int main(){
@@ -92,7 +178,7 @@ int main(){
   scanf("%c",&command);
 
   if(command == 'm') manualInput();
-  if(command == 'a') printf("Not yet implemented\n");
+  if(command == 'a') automaticInput();
 
   exit(0);
 }
