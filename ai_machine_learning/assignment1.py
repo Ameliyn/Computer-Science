3d753681430556304e8a90b7be0c2dd42efbbc8d
Ameliyn <39354564+Ameliyn@users.noreply.github.com>
2022-01-18 13:09:32 -0800

    Update assignment1.py

--- ai_machine_learning/assignment1.py
+++ ai_machine_learning/assignment1.py
@@ -2,13 +2,52 @@ def standard1(sentence):
     longestIndex = 0;
     i = 0;
     sentenceList = sentence.split();
-    print(sentenceList);
+    #print(sentenceList);
     while i < len(sentenceList):
         if(len(sentenceList[i]) > len(sentenceList[longestIndex])):
             longestIndex = i;
         i+=1;
+    #print("Longest index is " + str(longestIndex));
     print(sentenceList[longestIndex]);
 
+def standard2():
+  i = 99;
+  while i > 1:
+    print(str(i) + " bottles of beer on te wall\n" + str(i) + " bottles of beer")
+    print("Take one down, pass it around")
+    print(str(i-1) + " bottles of beer on the wall\n")
+    i-=1;
+  print("1 bottle of beer on the wall\n1 bottle of beer")
+  print("Take one down, pass it around")
+  print("0 bottles of beer on the wall!\n")
 
-print(standard1("Hello Worlds!"));
-print(standard1("A Be See Deed"));
+def advanced1(sentence):
+  sentenceList = sentence.split();
+  print("Inital sentence:")
+  print(sentenceList)
+  n = len(sentenceList)
+  for i in range(n-1):
+    for j in range(0, n-i-1):
+      if len(sentenceList[j]) > len(sentenceList[j+1]) or (len(sentenceList[j]) == len(sentenceList[j+1]) and sentenceList[j][0] > sentenceList[j+1][0]):
+        temp = sentenceList[j+1]
+        sentenceList[j+1] = sentenceList[j]
+        sentenceList[j] = temp
+
+  print("Sorted List:")  
+  print(sentenceList)
+
+def advanced2():
+  #display roman numerals
+  for i in range(100):
+    print("I")
+
+#Test the functions!
+#standard1("Hello World!");
+#standard1("A Be Seee Dee");
+#standard1("Here is a totally random sentence!")
+
+#standard2()
+
+#advanced1("B A totally random sentence with lots of words")
+
+advanced2()
