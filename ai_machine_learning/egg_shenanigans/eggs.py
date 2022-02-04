413ca3c2f500b0c0ca56c3588f9727a369354a89
Skye Russ <skyer@lclark.edu>
2022-02-04 14:47:01 -0800

    finished eggs.py <3

--- ai_machine_learning/egg_shenanigans/eggs.py
+++ ai_machine_learning/egg_shenanigans/eggs.py
@@ -11,10 +11,26 @@ with open('eggs_count.csv', newline='') as csvfile:
 
 # The keys for the rows are:
 #   Chicken: Name of the chicken (Latte, Mocha, Peppa, Snowy)
-#   Uncertain: Y if uncertain if chicken laid that egg, empty otherwise
+#   Uncertain: Y if uncertain is chicken laid that egg, empty otherwise
 #   Date: Date in MM/DD/YY format
 #   High: High temperature in F
 #   Low: Low temperature in F
 #   Weather: Clear, Mostly Cloudy, Overcast, Rain
 #   Breed: Americauna, Orpington
 # All values, even the temperatures, are stored as strings from the CSV
+
+egg_Counter = {}
+uncertainty_counter = 0
+for line in eggs:
+    if line["Chicken"] not in egg_Counter.keys() and line["Uncertain"] != 'Y':
+        egg_Counter[line["Chicken"]] = 1
+    else:
+        egg_Counter[line["Chicken"]] += 1
+
+    if line["Uncertain"] == 'Y':
+        uncertainty_counter += 1
+
+print("Peppa laid %d +/- %d eggs over the time period" % (egg_Counter["Peppa"], uncertainty_counter))
+print("Mocha laid %d +/- %d eggs over the time period" % (egg_Counter["Mocha"], uncertainty_counter))
+print("Snowy laid %d eggs over the time period" % (egg_Counter["Snowy"]))
+print("Dolce laid 0 +/- %d eggs over the time period" % uncertainty_counter)
