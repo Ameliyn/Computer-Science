2ed281cb926093a75673b8c39bc97a6a552e578a
Skye Russ <skyer@lclark.edu>
2022-02-14 14:19:45 -0800

    Recreated the random agent because they're easy. <3

--- ai_machine_learning/vacuum_world/vacuum_agents.py
+++ ai_machine_learning/vacuum_world/vacuum_agents.py
@@ -3,3 +3,21 @@
 Team 5: Simon Buan, Rosalee Hayes, Skye Russ, Anders Stall
 
 """
+# Possible actions: "clean", "north", "east", "south", "west"
+import random
+
+
+def reflex_action(space):
+    return "clean"
+
+
+def random_agent(space):
+    directions = ["north", "east", "south", "west"]
+    if space is True:
+        return random.choice(directions)
+    else:
+        return "clean"
+
+
+def state_agent(space):
+    return "clean"
