48b8b9761209c6dfcb1f8d6f277c4f4d74b9e952
Skye Russ <skyer@lclark.edu>
2022-02-16 14:50:35 -0800

    some work done on the final agent. <3

--- ai_machine_learning/vacuum_world/vacuum_agents.py
+++ ai_machine_learning/vacuum_world/vacuum_agents.py
@@ -5,17 +5,22 @@ Team 5: Simon Buan, Rosalee Hayes, Skye Russ, Anders Stall
 """
 # Possible actions: "clean", "north", "east", "south", "west"
 import random
+from vacuum import *
 
 
-def reflex_action(space):
-    if space is True:
+world = [100][100]
+current_xy = {50, 50}
+
+
+def reflex_agent(space):
+    if space is False:
         return "east"
     return "clean"
 
 
 def random_agent(space):
     directions = ["north", "east", "south", "west"]
-    if space is True:
+    if space is False:
         return random.choice(directions)
     else:
         return "clean"
@@ -25,3 +30,8 @@ def state_agent(space):
     if space is False:
         return "clean"
     return "east"
+
+
+run(20, 50000, random_agent)
+
+# print(many_runs(20, 50000, 10, random_agent))
