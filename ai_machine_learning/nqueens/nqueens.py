c45fa20804d7252b11d99a40e1f626844255ef50
Skye Russ <skyer@lclark.edu>
2022-02-21 14:54:32 -0800

    more work done on queeens. <3

--- ai_machine_learning/nqueens/nqueens.py
+++ ai_machine_learning/nqueens/nqueens.py
@@ -1,30 +1,47 @@
+# @author Skye Russ
 
-def initialize_board(num):
-    temp = [num][num]
-    for i in range(num):
-        for j in range(num):
-            temp[i][j] = 0
-    return temp
+"""
+If you've already place all n queens, return the solution
+Otherwise, for each row in the next column:
+If you can solve the puzzle by placing the next queen in that row and then recurring, return that solution
+If no solution was found, return False
+"""
 
 
-def check_space(num, x, y, board):
-    for i in range(num):
-        if board[i][y] == 1:
+def check_space(limit: int, r: int, c: int, board: tuple):
+    if r in board:
+        return False
+    if len(board) == 0:
+        return True
+    i = r - 1
+    while i >= 0:
+        if board[r - i] == c - i:
             return False
-        if board[x][i] == 1:
+        if board[r - i] == c + i:
             return False
-        if x+i < num and y+i < num and board[x+1][y+i] == 1:
-            return False
-        if x-i >= 0 and y-i >= 0 and board[x-1][y-i] == 1:
-            return False
-        if x+i < num and y-i >= 0 and board[x+1][y-i] == 1:
-            return False
-        if x-i >= 0 and y+i < num and board[x-1][y+i] == 1:
-            return False
+        i -= 1
     return True
 
 
-def nqueens(num):
-    board = initialize_board(num)
+def copy_tup(tup: tuple, n: int):
+    cp = ()
+    for i in tup:
+        cp += i
+    return cp
 
-    return False
+
+def find_solution(limit, c, queen_list):
+    if limit == c:
+        return queen_list
+    for r in range(limit):
+        if check_space(limit, r, c, queen_list):
+            temp = find_solution(limit, c + 1, copy_tup(queen_list, c))
+            if len(temp) <= 0 or temp[0] is False:
+                continue
+            else:
+                return temp
+    return False,
+
+
+def nqueens(num):
+    return find_solution(num, 0, ())
