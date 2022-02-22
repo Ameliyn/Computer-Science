58b6927e82b97d8aea7554fbc31134d04ee3b774
Skye Russ <skyer@lclark.edu>
2022-02-21 17:17:09 -0800

    fixed small weak errors. <3

--- ai_machine_learning/nqueens/nqueens.py
+++ ai_machine_learning/nqueens/nqueens.py
@@ -1,14 +1,14 @@
 # @author Skye Russ
 
 """
-If you've already place all n queens, return the solution
+If you've already placed all n queens, return the solution
 Otherwise, for each row in the next column:
 If you can solve the puzzle by placing the next queen in that row and then recurring, return that solution
 If no solution was found, return False
 """
 
 
-def check_space(limit: int, r: int, c: int, board: tuple):
+def check_space(r: int, c: int, board: tuple):
     if r in board:  # if row already taken
         return False
     i = 1  # start at 1 less than the current row
@@ -21,7 +21,7 @@ def check_space(limit: int, r: int, c: int, board: tuple):
 
 def find_solution(limit, c, queen_list):
     for r in range(limit):
-        if check_space(limit, r, c, queen_list):
+        if check_space(r, c, queen_list):
             if limit == c+1:  # if on last row
                 temp = queen_list + (r,)
             else:
