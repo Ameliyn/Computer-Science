f169c3462ed8b562d6ef543b9a46ae6c8e5ca082
Skye Russ <skyer@lclark.edu>
2022-02-28 14:25:37 -0800

    created winner function. <3

--- ai_machine_learning/tictactoe/tictactoe.py
+++ ai_machine_learning/tictactoe/tictactoe.py
@@ -1,4 +1,3 @@
-
 def successor(board, player, index):
     """
     Returns board if player played there or False if invalid move
@@ -7,7 +6,7 @@ def successor(board, player, index):
     :param index: where to put letter
     :return: string with successor move
     """
-    return board[:index] + player + board[index+1:]
+    return board[:index] + player + board[index + 1:]
 
 
 def legal_moves(board, player):
@@ -20,5 +19,22 @@ def legal_moves(board, player):
     return tuple([i for i in range(len(board)) if board[i] == "."])
 
 
+def winner(board):
+    """
+    Returns if the board has a winner, and who it is
+    :param board: string current board state
+    :return: 1 if "X" has won, -1 if "O" has won, or 0 otherwise
+    """
+    winning_lines = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for a, b, c in winning_lines:
+        if board[a] == board[b] == board[c]:
+            if board[a] == "X":
+                return 1
+            elif board[a] == "O":
+                return -1
+    return 0
+
+
 print(successor(".........", 'X', 0))
 print(legal_moves("..XX..OOX", "X"))
+print(winner("X.X..X..X"))
