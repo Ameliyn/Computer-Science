5199468e68c889dbd8b7b5ae147d20af322efe1e
Skye Russ <skyer@lclark.edu>
2022-03-07 14:25:08 -0800

    created the dataset for stats. <3

--- ai_machine_learning/math255/random_tester.py
+++ ai_machine_learning/math255/random_tester.py
@@ -10,13 +10,21 @@ def create_dataset(num_points, limit_a, limit_b, integer):
     :param integer: integer flag (1 for integers, 0 for doubles)
     :return:
     """
-    file_name = "python%d_%d_%d.txt" % (num_points, limit_a, limit_b)
-    with open(file_name, "w") as f:
+    # file_name = "python%d_%d_%d.txt" % (num_points, limit_a, limit_b)
+    file_name = "pythondataset.txt"
+    with open(file_name, "a") as f:
+        f.write("py_%d_%d_%d = " % (num_points, limit_a, limit_b))
         for i in range(num_points):
             if integer == 1:
-                print(random.randint(limit_a, limit_b), end=", ", file=f)
+                f.write(str(random.randint(limit_a, limit_b)))
+                f.write(", ")
+                # print(random.randint(limit_a, limit_b), end=", ", file=f)
             else:
-                print(random.random() * limit_b, end=", ", file=f)
+                f.write(str(random.random() * limit_b))
+                f.write(", ")
+                # print(random.random() * limit_b, end=", ", file=f)
+        f.write("\n")
+        # print(file=f)
 
 
 def main():
@@ -24,4 +32,7 @@ def main():
     params = ((0, 1, 0), (1, 128, 1), (1, 512, 1), (1, 1024, 1), (1, 2048, 1))
     for limit_a, limit_b, integer in params:
         for n in nums:
-            create_dataset(n, limit_a, limit_a, integer)
+            create_dataset(n, limit_a, limit_b, integer)
+
+
+main()
