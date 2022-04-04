f5880cc7d45be28e9d979a1ae1602a3ee9fe3b5d
Skye Russ <skyer@lclark.edu>
2022-04-04 14:50:02 -0700

    finished polynomial regression. <3

--- ai_machine_learning/polynomial_regression/polynomial.py
+++ ai_machine_learning/polynomial_regression/polynomial.py
@@ -1,10 +1,11 @@
 import numpy as np
-import random
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from statistics import mean
+
+# @author Skye Russ
 
 
 def generate_coefficients():
@@ -25,9 +26,9 @@ def generate_data(m, coefficients):
     where coefficients is (a, b, c, d, e) and the noise for each point is normally distributed with mean 0 and
     standard deviation 1.
     """
-    x = np.array(np.arange(-5.0, 5.00001, 10.0/(m-1))).reshape(m,1)
-    y = np.array(coefficients[0] + coefficients[1]*x + coefficients[2]*(x**2) + coefficients[3]*(x**3) +
-         coefficients[4]*(x**4) + np.random.standard_normal())
+    x = np.linspace(-5.0, 5.0, m).reshape(m, 1)
+    y = np.array(coefficients[0] + coefficients[1] * x + coefficients[2] * (x ** 2) + coefficients[3] * (x ** 3) +
+                 coefficients[4] * (x ** 4) + np.random.standard_normal())
     return x, y
 
 
@@ -59,7 +60,7 @@ def fit_curve(X, y, degree):
     X_poly = poly_features.fit_transform(X)
     lin_reg = LinearRegression()
     lin_reg.fit(X_poly, y)
-    return lin_reg.fit(X_poly, y)
+    return lin_reg
 
 
 def plot_curve(degree, model):
@@ -67,25 +68,26 @@ def plot_curve(degree, model):
     Plots a curve for model, which represents a polynomial of the specified degree.
     The x values for the curve are 100 points evenly spaced across the interval [-5.0, 5.0].
     """
-    x = np.array(np.arange(-5.0, 5.00001, 10.0 / (100 - 1)))
-    y = [model.intercept_ for i in range(100)]
-    coefs = model.coef_
-    print(coefs[0])
-    for j in range(degree):
-        for i in range(len(x)):
-            for k in range(degree):
-                y[i] += j*(x[i]**k)
-
-    plt.plot(x,y)
-    pass
+    # initialize x with uniformly distributed and y with intercept value
+    x = np.linspace(-5.0, 5.0, 100)
+    y = [model.intercept_[0] for i in range(100)]
+    # [0,limit] (range naturally goes from [0,limit)) so we must add 1 to degree
+    for i in range(degree + 1):
+        y += (x ** i) * model.coef_[0][i]
+    plt.plot(x, y, label=f"degree {degree}")
 
 
 def mse(X, y, degree, model):
     """
     Returns the mean squared error for model (a polynomial of the specified degree) on X and y.
     """
-    # TODO You have to write this. Start by removing the line below.
-    pass
+    # initialize y with the intercept
+    y_pred = [model.intercept_[0] for i in range(len(X))]
+    # [0,limit] (range naturally goes from [0,limit)) so we must add 1 to degree
+    for i in range(degree+1):
+        for j in range(len(X)):
+            y_pred[j] += (X[j][0] ** i) * model.coef_[0][i]
+    return mean_squared_error(y, y_pred)
 
 
 def experiment_1(m):
@@ -95,7 +97,7 @@ def experiment_1(m):
     coeffs = generate_coefficients()
     X, y = generate_data(m, coeffs)
     plot_data(X, y)
-    for d in [1, 2, 20]:
+    for d in [1, 2, 7,8,9,20]:
         model = fit_curve(X, y, d)
         plot_curve(d, model)
     plt.xlabel('x')
@@ -114,8 +116,9 @@ def experiment_2(m):
 
     After the 100 runs, plots the average mse of each degree.
     """
-    mses = {i : [] for i in range(1, 31)}
+    mses = {i: [] for i in range(1, 31)}
     for i in range(100):
+        print(f"Testing {i + 1} of 100")
         coeffs = generate_coefficients()
         X_train, y_train = generate_data(m, coeffs)
         X_test, y_test = generate_data(100, coeffs)
@@ -132,6 +135,10 @@ def experiment_2(m):
 
 if __name__ == '__main__':
     # generate_and_plot_data(100)
+    experiment_1(5)
     experiment_1(20)
+    experiment_1(50)
+    experiment_1(100)
+    experiment_1(200)
     # experiment_2(20)
     pass
