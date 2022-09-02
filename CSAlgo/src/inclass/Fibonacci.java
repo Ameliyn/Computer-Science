671bfe8bfce46de09227d3d9c2c9327e811130e4
Skye Russ <skyer@lclark.edu>
2022-09-02 13:40:20 -0700

    fast fibonacci done. <3

--- CSAlgo/src/inclass/Fibonacci.java
+++ CSAlgo/src/inclass/Fibonacci.java
@@ -1,16 +1,31 @@
 package inclass;
 import edu.princeton.cs.algs4.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Fibonacci {
 
     public static void main(String args[]){
         for(int n = 0; n < 10; n++){
-            StdOut.println("Fib(" + n + ") = " + fib(n));
+            StdOut.println("Fib(" + n + ") = " + fast_fib(n));
         }
+        StdOut.println("Fib(" + 500 + ") = " + fast_fib(500));
     }
 
+    //the speed is O(2^n)
     public static long fib(long n){
         if(n == 0 || n == 1) return n;
         return fib(n-1) + fib(n-2);
+    }
+
+    //By caching the value, the speed becomes O(n)
+    public static long[] cached = new long[1000];
+
+    public static long fast_fib(long n){
+        if(cached[(int)n] != 0) return cached[(int)n];
+        else if(n == 0 || n == 1) cached[(int)n] = n;
+        else cached[(int)n] = fast_fib(n-1) + fast_fib(n-2);
+        return cached[(int)n];
     }
 }
