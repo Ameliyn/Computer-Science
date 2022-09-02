606a6865afe6b3b091afbf22fab48c559a39bdca
Skye Russ <skyer@lclark.edu>
2022-09-02 13:11:45 -0700

    made a main function for fibonacci functions. <3

--- CSAlgo/src/inclass/Fibonacci.java
+++ CSAlgo/src/inclass/Fibonacci.java
@@ -3,7 +3,13 @@ import edu.princeton.cs.algs4.*;
 
 public class Fibonacci {
 
-    public long fib(long n){
+    public static void main(String args[]){
+        for(int n = 0; n < 10; n++){
+            StdOut.println("Fib(" + n + ") = " + fib(n));
+        }
+    }
+    
+    public static long fib(long n){
         if(n == 0 || n == 1) return n;
         return fib(n-1) + fib(n-2);
     }
