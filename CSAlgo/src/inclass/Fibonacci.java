0aa5296dedc3bd810a586b287645b718191533dc
Skye Russ <skyer@lclark.edu>
2022-09-07 13:16:24 -0700

    created fast and small fibonacci. <3

--- CSAlgo/src/inclass/Fibonacci.java
+++ CSAlgo/src/inclass/Fibonacci.java
@@ -4,10 +4,9 @@ import edu.princeton.cs.algs4.*;
 public class Fibonacci {
 
     public static void main(String args[]){
-        for(int n = 0; n < 10; n++){
-            StdOut.println("Fib(" + n + ") = " + fast_fib(n));
+        for(int n = 0; n < 95; n++){
+            StdOut.println("Fib(" + n + ") = " + fast_small_fib(n));
         }
-        StdOut.println("Fib(" + 500 + ") = " + fast_fib(500));
     }
 
     //the speed is O(2^n)
@@ -19,10 +18,22 @@ public class Fibonacci {
     //By caching the value, the speed becomes O(n)
     public static long[] cached = new long[1000];
 
+    //O(n) speed with O(n) space
     public static long fast_fib(long n){
         if(cached[(int)n] != 0) return cached[(int)n];
         else if(n == 0 || n == 1) cached[(int)n] = n;
         else cached[(int)n] = fast_fib(n-1) + fast_fib(n-2);
         return cached[(int)n];
+    }
+
+    //O(n) speed with less than O(n) space
+    public static long fast_small_fib(long n){
+        return fast_small_fib_helper(n,0,1);
+    }
+
+    private static long fast_small_fib_helper(long n, long a, long b){
+        if(n == 0) return a;
+        else if (n==1) return b;
+        return fast_small_fib_helper(n-1, b, a+b);
     }
 }
