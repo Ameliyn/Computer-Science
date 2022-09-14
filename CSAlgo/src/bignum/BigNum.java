4603ffba7ba45044c44239ff2cd8a94b503ca0be
Skye Russ <skyer@lclark.edu>
2022-09-14 11:15:55 -0700

    plus complete, but others not. <3

--- CSAlgo/src/bignum/BigNum.java
+++ CSAlgo/src/bignum/BigNum.java
@@ -1,5 +1,7 @@
 package bignum;
 
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.ArrayList;
 
 public class BigNum {
@@ -8,7 +10,8 @@ public class BigNum {
     String strNum;
 
     public BigNum(String number){
-        strNum = number;
+        if(number.equals("")) strNum = "0";
+        else strNum = number;
         charlist = new ArrayList<Character>();
         for(char c: number.toCharArray()){
             charlist.add(c);
@@ -32,12 +35,12 @@ public class BigNum {
         int i = charlist.size()-1;
         int j = bigs.charlist.size()-1;
 
-        while(i > 0 || j > 0){
-            if(i == 0){
+        while(!(j==-1 && i==-1)){
+            if(i < 0 && j>=0){
                 tempSum = carry + bigs.charlist.get(j) - 48;
                 j--;
             }
-            else if(j == 0){
+            else if(j < 0 && i>=0){
                 tempSum = carry + charlist.get(i) - 48;
                 i--;
             }
@@ -49,12 +52,22 @@ public class BigNum {
             if(tempSum / 10 != 0) carry = 1;
             else carry = 0;
 
-            result = (tempSum%10) + result;
+            result = ((tempSum)%10) + result;
         }
 
         if(carry == 1) result = '1' + result;
 
 
         return new BigNum(result);
+    }
+
+    public static void main(String[] args){
+        String inp = "12345";
+        ArrayList<Character> newcharlist = new ArrayList<Character>();
+        for(char c: inp.toCharArray()){
+            newcharlist.add(c);
+        }
+
+        StdOut.println(new BigNum("1234").plus(new BigNum("111")));
     }
 }
