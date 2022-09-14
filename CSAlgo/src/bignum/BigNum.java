2312bb2d15be694bd4703b0986197d3e90c07a6d
Skye Russ <skyer@lclark.edu>
2022-09-14 13:42:01 -0700

    started work on speeding up the times function. <3

--- CSAlgo/src/bignum/BigNum.java
+++ CSAlgo/src/bignum/BigNum.java
@@ -2,6 +2,8 @@ package bignum;
 
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+
 public class BigNum {
 
     String number;
@@ -16,6 +18,47 @@ public class BigNum {
         return number;
     }
 
+    //this is an attempt at speeding up the times function
+    public BigNum times2(BigNum other){
+        if(other.number.length() == 0 || number.length() == 0) return new BigNum("0");
+
+        BigNum result = new BigNum("");
+
+        char[] selfChars = number.toCharArray();
+        char[] otherChars = other.number.toCharArray();
+        int carry = 0;
+        ArrayList<Character> resultList = new ArrayList<Character>();
+        int selfZeroes = 0; int otherZeroes = 0;
+
+        for(int i = number.length()-1; i >= 0; i--){
+
+            otherZeroes = 0;
+            for(int j = other.number.length()-1; j >= 0; j--){
+                //find current product
+                int prod = (selfChars[i] - '0') * (otherChars[j] - '0') + carry;
+
+                //find new carry (10s place of product)
+                carry = prod / 10;
+
+                //find where the 0s place of the product will be (0s place, 10s place, 100s place, etc)
+                int index = selfZeroes + otherZeroes;
+
+                //TODO: add to the correct index
+
+                //get the string representation of the current number
+                String tempResult = Integer.toString(prod) +
+                        "0".repeat(Math.max(0, other.number.length() - j - 1)) +
+                        "0".repeat(Math.max(0, number.length() - i - 1));
+
+                result = result.plus(new BigNum(tempResult));
+                otherZeroes++;
+            }
+            selfZeroes++;
+        }
+        return result;
+    }
+
+
     public BigNum times(BigNum other){
         if(other.number.length() == 0 || number.length() == 0) return new BigNum("0");
 
@@ -23,21 +66,14 @@ public class BigNum {
 
         char[] selfChars = number.toCharArray();
         char[] otherChars = other.number.toCharArray();
-
         for(int i = number.length()-1; i >= 0; i--){
             for(int j = other.number.length()-1; j >= 0; j--){
-                Integer prod = (selfChars[i] - '0') * (otherChars[j] - '0');
-                String tempResult = prod.toString();
+                int prod = (selfChars[i] - '0') * (otherChars[j] - '0');
 
-                //add the 0s for the other
-                for(int k = 0; k < other.number.length()-j-1; k++){
-                    tempResult += "0";
-                }
-
-                //add the 0s for the self
-                for(int k = 0; k < number.length()-i-1; k++){
-                    tempResult += "0";
-                }
+                //get the string representation of the current number
+                String tempResult = Integer.toString(prod) +
+                        "0".repeat(Math.max(0, other.number.length() - j - 1)) +
+                        "0".repeat(Math.max(0, number.length() - i - 1));
 
                 result = result.plus(new BigNum(tempResult));
             }
@@ -48,7 +84,7 @@ public class BigNum {
     public BigNum plus(BigNum other){
         int carry = 0;
         int tempSum;
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         int i = number.length()-1;
         int j = other.number.length()-1;
@@ -73,22 +109,12 @@ public class BigNum {
             if(tempSum / 10 != 0) carry = 1;
             else carry = 0;
 
-            result = ((tempSum)%10) + result;
+            result.insert(0, ((tempSum) % 10));
         }
 
-        if(carry == 1) result = '1' + result;
+        if(carry == 1) result.insert(0, '1');
 
 
-        return new BigNum(result);
-    }
-
-    public static void main(String[] args){
-        String inp = "300000000000";
-        String inp2 = "400000000000";
-
-        BigNum one = new BigNum(inp);
-        BigNum two = new BigNum(inp2);
-
-        StdOut.println(one.times(two));
+        return new BigNum(result.toString());
     }
 }
