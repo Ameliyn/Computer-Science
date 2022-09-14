1339a8b0b28df11327f66a5476d4a3e5704af55f
Skye Russ <skyer@lclark.edu>
2022-09-14 13:01:38 -0700

    technically times works. <3

--- CSAlgo/src/bignum/BigNum.java
+++ CSAlgo/src/bignum/BigNum.java
@@ -2,50 +2,71 @@ package bignum;
 
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.ArrayList;
-
 public class BigNum {
 
-    ArrayList<Character> charlist;
-    String strNum;
+    String number;
 
     public BigNum(String number){
-        if(number.equals("")) strNum = "0";
-        else strNum = number;
-        charlist = new ArrayList<Character>();
-        for(char c: number.toCharArray()){
-            charlist.add(c);
-        }
+        if(number.equals("")) this.number = "0";
+        else this.number = number;
     }
 
     @Override
     public String toString(){
-        return strNum;
+        return number;
     }
 
-    public BigNum times(BigNum bigs){
-        return bigs;
+    public BigNum times(BigNum other){
+        if(other.number.length() == 0 || number.length() == 0) return new BigNum("0");
+
+        BigNum result = new BigNum("");
+
+        char[] selfChars = number.toCharArray();
+        char[] otherChars = other.number.toCharArray();
+
+        for(int i = number.length()-1; i >= 0; i--){
+            for(int j = other.number.length()-1; j >= 0; j--){
+                Integer sum = (selfChars[i] - 48) * (otherChars[j] - 48);
+                String tempResult = sum.toString();
+
+                //add the 0s for the other
+                for(int k = 0; k < other.number.length()-j-1; k++){
+                    tempResult += "0";
+                }
+
+                //add the 0s for the self
+                for(int k = 0; k < number.length()-i-1; k++){
+                    tempResult += "0";
+                }
+
+                result = result.plus(new BigNum(tempResult));
+            }
+        }
+        return result;
     }
 
-    public BigNum plus(BigNum bigs){
+    public BigNum plus(BigNum other){
         int carry = 0;
         int tempSum;
         String result = "";
 
-        int i = charlist.size()-1;
-        int j = bigs.charlist.size()-1;
+        int i = number.length()-1;
+        int j = other.number.length()-1;
+
+        char[] selfChars = number.toCharArray();
+        char[] otherChars = other.number.toCharArray();
 
         while(!(j==-1 && i==-1)){
             if(i < 0 && j>=0){
-                tempSum = carry + bigs.charlist.get(j) - 48;
+                tempSum = carry + otherChars[j] - 48;
                 j--;
             }
             else if(j < 0 && i>=0){
-                tempSum = carry + charlist.get(i) - 48;
+                tempSum = carry + selfChars[i] - 48;
                 i--;
             }
             else {
-                tempSum = carry + bigs.charlist.get(j) + charlist.get(i) - 48*2;
+                tempSum = carry + otherChars[j] + selfChars[i] - 48*2;
                 i--;
                 j--;
             }
@@ -62,12 +83,12 @@ public class BigNum {
     }
 
     public static void main(String[] args){
-        String inp = "12345";
-        ArrayList<Character> newcharlist = new ArrayList<Character>();
-        for(char c: inp.toCharArray()){
-            newcharlist.add(c);
-        }
+        String inp = "300000000000";
+        String inp2 = "400000000000";
 
-        StdOut.println(new BigNum("1234").plus(new BigNum("111")));
+        BigNum one = new BigNum(inp);
+        BigNum two = new BigNum(inp2);
+
+        StdOut.println(one.times(two));
     }
 }
