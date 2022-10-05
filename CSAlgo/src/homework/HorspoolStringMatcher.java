6fded7611ec489717e14fd2b1fceb77e5d85920a
Skye Russ <skyer@lclark.edu>
2022-10-05 12:36:51 -0700

    ceated the framework for the horspool problem. <3

---
+++ CSAlgo/src/homework/HorspoolStringMatcher.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class HorspoolStringMatcher {
+
+    public HorspoolStringMatcher(String substring){
+
+    }
+
+    public int getShift(char c){
+        return 0;
+    }
+
+    public int match(String target){
+        return 0;
+    }
+}
