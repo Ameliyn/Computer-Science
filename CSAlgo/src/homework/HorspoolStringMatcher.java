63946f5444a1cef5eec86f1df56d8ae0928f092c
Skye Russ <skyer@lclark.edu>
2022-10-05 13:42:59 -0700

    more framework. <3

--- CSAlgo/src/homework/HorspoolStringMatcher.java
+++ CSAlgo/src/homework/HorspoolStringMatcher.java
@@ -3,11 +3,20 @@ package homework;
 public class HorspoolStringMatcher {
 
     private char[] needle;
+    private int[] shiftTable;
 
     public HorspoolStringMatcher(String needle){
         this.needle = needle.toCharArray();
+        GenerateShiftTable();
     }
 
+    private void GenerateShiftTable(){
+        shiftTable = new int[256];
+        for(int i = 0; i < 257; i++){
+            shiftTable[i] = getShift((char)i);
+        }
+    }
+    
     /**
      * returns how many you can jump before the next occurrence of this letter
      * @param c
