e5afe4d83edcf5461f992c27a7c7cdfaca286b75
Skye Russ <skyer@lclark.edu>
2022-10-19 13:36:01 -0700

    bloom filter is not crashing. <3

--- CSAlgo/src/bloom/BloomFilter.java
+++ CSAlgo/src/bloom/BloomFilter.java
@@ -5,7 +5,7 @@ public class BloomFilter<k> {
     private long[] bloom;
 
     public BloomFilter(){
-        bloom = new long[1024];
+        bloom = new long[65536/64];
     }
 
     /**
@@ -14,7 +14,10 @@ public class BloomFilter<k> {
      * @return
      */
     public boolean mightContain(k s) {
-        return false;
+        //find if any bits in the hashed value are set
+        int h1 = hashHigh(s);
+        int h2 = hashLow(s);
+        return get(h1) || get(h2);
     }
 
     /**
@@ -22,8 +25,23 @@ public class BloomFilter<k> {
      * @param s object to add
      */
     public void add(k s) {
-        hashLow(s);
-        hashHigh(s);
+        int h1 = hashLow(s);
+        int h2 = hashHigh(s);
+
+        set(h1);
+        set(h2);
+    }
+
+    private void set(int i){
+        int spot1 = i / 64;
+        int offset1 = i % 64;
+        bloom[spot1] |= (1 >> offset1);
+    }
+
+    private boolean get(int i){
+        int spot = i / 64;
+        int offset = i % 64;
+        return (bloom[spot] >> offset & 0x1) == 1;
     }
 
     /**
@@ -43,9 +61,11 @@ public class BloomFilter<k> {
      * @param s
      * @return
      */
-    private int hashLow(k s){
-        int lowSixteen = s.hashCode() << 16 >> 16;
-        return 0;
+    private int hashHigh(k s){
+        int highSixteen = s.hashCode() >>> 16;
+
+        //somehow make the lowSixteen number between [0,65536]
+        return highSixteen;
     }
 
     /**
@@ -53,17 +73,20 @@ public class BloomFilter<k> {
      * @param s
      * @return
      */
-    private int hashHigh(k s){
-        int highSixteen = s.hashCode() >> 16;
-        return 0;
+    private int hashLow(k s){
+        int lowSixteen = s.hashCode() << 16 >>> 16;
+
+        //somehow make the highSixteen number between [0,65536]
+        return lowSixteen;
     }
 
     public static void main(String[] unused){
-        int hash = 0x12345678;
-        int highSixteen = hash >> 16;
-        int lowSixteen = (hash << 16) >> 16;
-
-        System.out.println(String.format("0x%08X", lowSixteen));
-        System.out.println(String.format("0x%08X", highSixteen));
+        int h = 0x87654321;
+        int low = h >>> 16;
+        int high = h << 16 >> 16;
+        System.out.println(low/64);
+        System.out.println(String.format("0x%08X", low));
+        System.out.println(high/64);
+        System.out.println(String.format("0x%08X", high));
     }
 }
--- CSAlgo/src/inclass/BinaryTree.java
+++ CSAlgo/src/inclass/BinaryTree.java
@@ -1,5 +1,6 @@
 package inclass;
 
+import com.sun.jdi.connect.Connector;
 import edu.princeton.cs.algs4.StdOut;
 
 public class BinaryTree {
@@ -13,39 +14,83 @@ public class BinaryTree {
             this.value = value;
         }
 
-        @Override
-        public String toString(){
-            String result = Integer.toString(value);
-            if(left != null) result += ":{" + left;
-            else if(right != null) result += ":{";
-
-            if(left != null && right != null) result += ",";
-
-            if(right != null) result += right + "}";
-            else if(left != null) result += "}";
-            return result;
+        public String toString() {
+            String s = "{:" + value;
+            if (left != null) s += left.toString();
+            if (right != null) s += right.toString();
+            return s + "}";
         }
     }
 
     private Node root;
 
     public BinaryTree() {
-        root = new Node(5);
-        root.left = new Node(1);
-        root.right = new Node(8);
-        root.left.left = new Node(2);
-        root.left.right = new Node(7);
-        root.right.left = new Node(4);
-        root.right.right = new Node(3);
+        root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
     }
 
     public String toString() {
         return root.toString();
     }
 
+    public void traverse_inorder() {
+        traverse_inorder(root);
+    }
+
+    private void traverse_inorder(Node n) {
+        if (n != null) {
+            traverse_inorder(n.left);
+            StdOut.print(n.value);
+            traverse_inorder(n.right);
+        }
+    }
+
+    public void traverse_postorder() {
+        traverse_postorder(root);
+    }
+
+    private void traverse_postorder(Node n) {
+        if (n != null) {
+            traverse_postorder(n.left);
+            traverse_postorder(n.right);
+            StdOut.print(n.value);
+        }
+    }
+
+    private void traverse_preorder(){
+        traverse_preorder(root);
+    }
+
+    private void traverse_preorder(Node n){
+        if(n != null){
+            StdOut.print(n.value);
+            traverse_preorder(n.left);
+            traverse_preorder(n.right);
+        }
+    }
+
+    private void traverse_breadthFirst(){
+        traverse_breadthFirst(root);
+    }
+
+    private void traverse_breadthFirst(Node n){
+        if(n != null){
+
+        }
+    }
     public static void main(String[] unused) {
         BinaryTree t = new BinaryTree();
-        StdOut.println(t.toString());
+        //StdOut.println(t.toString());
+        t.traverse_inorder();
+        StdOut.println("\nPostOrder");
+        t.traverse_postorder();
+        StdOut.println("\nPreOrder");
+        t.traverse_preorder();
     }
 
 }
