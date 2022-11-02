49127020cd6c150874344cf74464c131209df2d8
Skye Russ <skyer@lclark.edu>
2022-11-02 13:25:39 -0700

    started the framework for the team project. <3

---
+++ CSAlgo/src/pattern/BruteForceMatcher.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class BruteForceMatcher implements Matcher {
+
+    public BruteForceMatcher(){
+
+    }
+
+    @Override
+    public int[] match(int[][] pattern, int[][] text) {
+        return new int[0];
+    }
+}
---
+++ CSAlgo/src/pattern/FingerprintMatcher.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class FingerprintMatcher implements Matcher{
+
+    public FingerprintMatcher(){
+
+    }
+    
+    @Override
+    public int[] match(int[][] pattern, int[][] text) {
+        return new int[0];
+    }
+}
