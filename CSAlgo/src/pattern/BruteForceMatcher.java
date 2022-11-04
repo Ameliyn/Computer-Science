1b02b404e13a61da5cc7d176507d338b16b41b60
Skye Russ <skyer@lclark.edu>
2022-11-04 13:06:05 -0700

    some work done. <3

--- CSAlgo/src/pattern/BruteForceMatcher.java
+++ CSAlgo/src/pattern/BruteForceMatcher.java
@@ -8,6 +8,21 @@ public class BruteForceMatcher implements Matcher {
 
     @Override
     public int[] match(int[][] pattern, int[][] text) {
-        return new int[0];
+        boolean match = true;
+        int[] result = new int[2];
+
+        for(int i = 0; i <= (text.length - pattern.length); i++){
+            for(int j = 0; j <= (text[0].length - pattern[0].length); j++){
+                for(int patI = 0; patI < pattern.length; patI++){
+                    for(int patJ = 0; patJ < pattern[patI].length; patJ++){
+                        if(text[i + patI][j + patJ] != pattern[patI][patJ]) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
     }
 }
--- CSAlgo/src/pattern/FingerprintMatcher.java
+++ CSAlgo/src/pattern/FingerprintMatcher.java
@@ -5,7 +5,7 @@ public class FingerprintMatcher implements Matcher{
     public FingerprintMatcher(){
 
     }
-    
+
     @Override
     public int[] match(int[][] pattern, int[][] text) {
         return new int[0];
