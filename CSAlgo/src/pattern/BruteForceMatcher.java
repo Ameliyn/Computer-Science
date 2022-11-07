6633b5e499c05ed9db0e2f6398a59af28b6a3e8b
Skye Russ <skyer@lclark.edu>
2022-11-07 12:52:12 -0800

    brute force matcher complete. <3

--- CSAlgo/src/pattern/BruteForceMatcher.java
+++ CSAlgo/src/pattern/BruteForceMatcher.java
@@ -8,21 +8,24 @@ public class BruteForceMatcher implements Matcher {
 
     @Override
     public int[] match(int[][] pattern, int[][] text) {
-        boolean match = true;
-        int[] result = new int[2];
-
         for(int i = 0; i <= (text.length - pattern.length); i++){
             for(int j = 0; j <= (text[0].length - pattern[0].length); j++){
-                for(int patI = 0; patI < pattern.length; patI++){
-                    for(int patJ = 0; patJ < pattern[patI].length; patJ++){
-                        if(text[i + patI][j + patJ] != pattern[patI][patJ]) {
-                            break;
-                        }
-                    }
-                }
+                if(CheckSection(pattern, text, i, j)) return new int[]{i,j};
             }
         }
+        return null;
+    }
 
-        return result;
+    private boolean CheckSection(int[][] pattern, int[][] text, int i, int j){
+        int initialJ = j;
+        for(int k = 0; k < pattern.length; k++){
+            for(int l = 0; l < pattern[0].length; l++){
+                if(text[i][j] != pattern[k][l]) return false;
+                j++;
+            }
+            i++;
+            j = initialJ;
+        }
+        return true;
     }
 }
