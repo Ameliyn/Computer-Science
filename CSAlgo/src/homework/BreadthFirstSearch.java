4d4274e7fe41b8c4bb0856c786dd92fd05464486
Skye Russ <skyer@lclark.edu>
2022-11-20 16:10:26 -0800

    I THINK ITS RIGHT BUT ITS OBVIOUSLY WRONG. <3

--- CSAlgo/src/homework/BreadthFirstSearch.java
+++ CSAlgo/src/homework/BreadthFirstSearch.java
@@ -2,6 +2,8 @@ package homework;
 
 import edu.princeton.cs.algs4.*;
 
+import java.util.ArrayDeque;
+
 public class BreadthFirstSearch {
 
     public static Digraph readG(String filename) {
@@ -16,22 +18,38 @@ public class BreadthFirstSearch {
         }
         return g;
     }
-
+    
     public static void main(String[] args) {
         Digraph g = readG(args[0]);
-        StdOut.println(g);
+        for(int i = 0; i < g.V(); i++){
+            StdOut.print(BFS(g, i, 0) + " ");
+        }
     }
 
-    private java.util.ArrayList<Integer> visitedLocations = new java.util.ArrayList<Integer>();
-    public static int shortestPath(Digraph g, int to, int from){
-        int numberSteps = 0;
-        if(to == from) return numberSteps;
-        else{
-            //for everything adjacent to us
+    public static int BFS(Digraph g, int to, int from)
+    {
+        if(g.indegree(to) == 0) return -1;
+        int[] weights = new int[g.V()];
+        weights[from] = 0;
+        Queue<Integer> q =  new Queue<>();
+
+
+        q.enqueue(from);
+        boolean[] discovered = new boolean[g.V()];
+        while(!q.isEmpty()){
+            if(to == from) return weights[from];
+            discovered[from] = true;
+
+            //if not the right place, enqueue the adjacent values
             for(int c: g.adj(from)){
-                numberSteps = 1 + shortestPath(g, to, c);
+                if(!discovered[c]){
+                    weights[c] = weights[from] + 1;
+                    q.enqueue(c);
+                }
             }
+            from = q.dequeue();
         }
-        return numberSteps;
+
+        return -1;
     }
 }
